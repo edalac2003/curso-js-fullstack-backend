b46780ee9f60d9b62e5f99c50eae3654b1d86d7e
edwin.acosta <edwin.acosta@quipux.com>
2019-07-23 17:03:01 -0500

    actualizacion servicesController.js

--- spafeliz-api/controllers/servicesController.js
+++ spafeliz-api/controllers/servicesController.js
@@ -3,10 +3,10 @@ var serviceModel = require('../models/serviceModel');
 
 exports.services_all_get = function(req, res){
     serviceModel.findAll({
-        attribute: ['id', 'name', 'image', 'happy']
+        attribute: ['id', 'name', 'happy', 'image']
     })
     .then(data => res.json(data))
-    .catch( err => res.status(500, err))
+    .catch( err => res.status(500).send(err))
 }
 
 exports.services_detail_get = function(req, res){
